404aa40f5eaeb867b64ee868de01ad49a7ad8560
user.name <anselmodiasyahoo@gmail.com>
2023-04-06 20:47:04 -0300

    complete project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Title } from "./Title";
+
+// Se você estiver retornando mais de um elemento do component, é preciso ter um elemento pai por volta.
+export function App() {
+  return (
+   <>
+    <Title />
+    <Title />
+   </>
+  )
+}
+
---
+++ src/Title.tsx
@@ -0,0 +1,7 @@
+// Esse aqui é um exemplo de um component - Uma função que retorna html
+// * Para você conseguir chamar esse component em outros lugares é preciso exportar a função.
+export function Title() {
+  return (
+    <h1>Hello Wordl</h1>
+  )
+}
