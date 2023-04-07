b36cdcd553210885d9108c1f3daff2b7f77e6c4e
user.name <anselmodiasyahoo@gmail.com>
2023-04-07 19:20:42 -0300

    ..

--- src/Title.tsx
+++ src/Title.tsx
@@ -1,5 +1,6 @@
 // Esse aqui é um exemplo de um component - Uma função que retorna html
-// * Para você conseguir chamar esse component em outros lugares é preciso exportar a função.
+// * Para você conseguir chamar esse component em outros lugares é preciso exportar a função. 
+
 export function Title() {
   return (
     <h1>Hello Wordl</h1>
